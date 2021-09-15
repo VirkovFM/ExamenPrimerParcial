3eef6f8ad63e2ef2d250ddc3a01a7923c29ffd91
Virkov <vfm72673@udelasalle.edu.mx>
2021-09-14 22:48:54 -0500

    first examt

---
+++ js/formula.js
@@ -0,0 +1,27 @@
+document.write("<h1>Formula general</>")
+
+function calcular(){
+
+    if (document.getElementById("a").value=="" || document.getElementById("b").value=="" || document.getElementById("c").value==""){
+        alert("Introduce un valor correcto")
+    }
+    else{
+        var a=document.getElementById("a").value;
+        var b= document.getElementById("b").value;
+        var c= document.getElementById("c").value;
+   
+   var an = Number(a);
+   var bn = Number(b);
+   var cn = Number(c);
+    if( isNaN(an) || isNaN(bn) || isNaN(cn)){
+        alert("Introduce un valor numerico")
+    }
+    else{
+        var x1 = (((-1)*bn)-(Math.sqrt(Math.pow(bn,2))-(4*an*cn)))/(2*an);
+        var x2 = (((-1)*bn)+(Math.sqrt(Math.pow(bn,2))-(4*an*cn)))/(2*an);
+        document.getElementById("X1").innerHTML= x1
+        document.getElementById("X2").innerHTML= x2
+    }
+    
+}
+}
---
+++ js/serie.js
@@ -0,0 +1,29 @@
+document.write("<h1>Serie de fibonacci</>")
+   
+function fibonacci(numero)
+{
+    let numeros=[0,1];
+    for (let i = 2; i < numero; i++) {
+        numeros[i] = numeros[i - 2] + numeros[i - 1];
+    }
+    return numeros;
+}
+
+function calcular(){
+    if (document.getElementById("cantidad").value=="" || document.getElementById("cantidad").value==0){
+        alert("Introduce un valor correcto")
+    }
+    else{
+   var nString = document.getElementById("cantidad").value;
+   
+   var n = Number(nString);
+    if( isNaN(n)){
+        alert("Introduce un valor numerico")
+    }
+    else{
+    var cadena = fibonacci(n)
+
+   document.getElementById("resultados").innerHTML= cadena
+    }
+    }
+}
